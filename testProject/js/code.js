596f64fa8d4898056dd8991ae4c4093ca89cfafd
chickinnick <chickinnick@ya.ru>
2017-10-28 05:35:15 +0300

    center block banner lg

--- testProject/js/code.js
+++ testProject/js/code.js
@@ -1,3 +1,13 @@
-/**
- * Created by Nick on 25.10.2017.
- */
+var MyBanners = new Array('images/banner/banner1.jpg',
+    'images/banner/banner2.jpg', 'images/banner/banner3.jpg', 'images/banner/banner4.jpg');
+var banner = 0;
+function ShowBanners() {
+    if (document.images) {
+        banner++;
+        if (banner == MyBanners.length) {
+            banner = 0;
+        }
+        document.ChangeBanner.src = MyBanners[banner];
+        setTimeout("ShowBanners()", 5000);
+    }
+}
